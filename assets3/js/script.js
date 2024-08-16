8404cb7e327120cc5897156387de645d298422fa
Anima <cjaccardh@gmail.com>
2024-08-16 14:27:51 -0400

    Script from the form is done. Still missing code from the finish button

--- assets3/js/script.js
+++ assets3/js/script.js
@@ -4,20 +4,50 @@ var tareas = [
     { tarea: "Pagar la tarjeta de crédito" }
 ]
 
+//Formulario
 let form = document.getElementById("formulario");
 let btnNew = document.getElementById("btnNew");
-let btnAdd = document.getElementById("btnAdd");
 let newTxt = document.getElementById("nuevaTarea");
-let tableBody = document.getElementById("cuerpo-tabla");
+let btnAdd = document.getElementById("btnAdd");
 
-const row = document.createElement('tr')
+//Tabla
+let tBody = document.getElementById("cuerpo-tabla");
 
-// "Agregar tarea" despliega text input
-btnNew.addEventListener('click', function(){
+
+//"Agregar tarea" despliega text input
+btnNew.addEventListener('click', function () {
     form.style.display = "block";
 })
 
-// "Agregar" introduce el text input en la tabla/lista
-btnAdd.addEventListener('click', function(){
-    form.style.display = "none";
+//Función que toma la información de la tabla y la actualiza
+
+function tUpdate() {
+    tBody.innerHTML = '';
+    tareas.forEach(function (tareaObj) {
+        const row = document.createElement('tr');
+        const data = document.createElement('td');
+        const finish = document.createElement('td');
+        const btnFinish = document.createElement('button');
+        data.textContent = tareaObj.tarea;
+        btnFinish.setAttribute('class', 'btn btn-danger');
+        btnFinish.textContent = "Finalizar";
+        finish.appendChild(btnFinish);
+        row.appendChild(data);
+        row.appendChild(finish);
+        tBody.appendChild(row);
+    });
+}
+
+//"Agregar" introduce el text input en la tabla/lista
+btnAdd.addEventListener('click', function () {
+    if (newTxt.value.trim() !== ''){
+        tareas.push({tarea: newTxt.value});
+        tUpdate();
+        form.style.display = "none";
+        newTxt.value = '';
+    }
 })
+
+//Boton "finalizar" para borrar elementos de la tabla/lista
+
+tUpdate()

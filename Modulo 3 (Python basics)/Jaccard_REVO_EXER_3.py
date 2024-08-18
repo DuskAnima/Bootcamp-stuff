5f7a47c12b8417bb7a7564da6e611bf4beca86cf
Anima <cjaccardh@gmail.com>
2024-08-18 14:29:23 -0400

    Literally a multiplication with py

---
+++ Modulo 3 (Python basics)/Jaccard_REVO_EXER_3.py
@@ -0,0 +1,4 @@
+a = 20 #Se establece variable a.
+b = 30 #Se establece variable b.
+resultado = a*b #Se asigna la operación (multiplicación) de ambos números a una variable llamada "resultado".
+print(resultado) #print() recoje la variable "resultado" para que imprima el resultado en consola.

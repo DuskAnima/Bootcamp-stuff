0e2ad0cad4b04f9bab3e83fe18e614064b754dbc
Anima <cjaccardh@gmail.com>
2024-08-18 00:38:18 -0400

    jQuery toggle demo.

---
+++ assets4/js/script.js
@@ -0,0 +1,17 @@
+$("#Baires").click(function () {
+  $("#detallesBaires").toggle();
+});
+
+$("#MachuPicchu").click(function () {
+  $("#detallesMachuPicchu").toggle();
+});
+
+$("#Rio").click(function () {
+  $("#detallesRio").toggle();
+});
+
+///////////////////////////////////////////////////////
+
+$(".btn-close").click(function () {
+  $(".detalles").hide();
+});

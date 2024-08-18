6afe763189d37a942d55d6b45e86e114e6051f8f
Anima <cjaccardh@gmail.com>
2024-08-18 15:49:26 -0400

    Literally a list of dictionaries

---
+++ Modulo 3 (Python basics)/Jaccard_Evaluacion_4.py
@@ -0,0 +1,7 @@
+persona1 = {"nombre":"Jorge", "apellido":"Borges", "Edad":"30"}
+persona2 = {"nombre":"María", "apellido":"Bombal", "Edad":"35"}
+persona3 = {"nombre":"Juan", "apellido":"Rulfo", "Edad":"31"}
+
+personas = [persona1, persona2, persona3]
+
+print(personas)

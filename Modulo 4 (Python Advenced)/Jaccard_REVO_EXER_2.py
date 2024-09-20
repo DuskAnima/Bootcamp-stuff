e574c94fed3a72185b072fcc3d79b33c51276e62
Anima <cjaccardh@gmail.com>
2024-09-20 16:48:07 -0300

    Learning Python Class stuff

---
+++ Modulo 4 (Python Advenced)/Jaccard_REVO_EXER_2.py
@@ -0,0 +1,10 @@
+class Animal:
+    def __init__(self, nombre, edad, raza, peso):
+        self.nombre = nombre
+        self.raza = raza
+        self.edad = edad
+        self.peso = peso
+
+caballo = Animal("Zeus", 5, "Pura Sangre", 450)
+leon = Animal("Boulder", 10, "Atlas", 130)
+

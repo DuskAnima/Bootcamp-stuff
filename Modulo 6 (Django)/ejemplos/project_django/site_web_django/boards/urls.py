4ae4c064c2cf538721b68aef0a1be0a6d5112ee6
Anima <cjaccardh@gmail.com>
2024-11-18 02:51:56 -0300

    palindrome checker. Only the view is made

--- Modulo 6 (Django)/ejemplos/project_django/site_web_django/boards/urls.py
+++ Modulo 6 (Django)/ejemplos/project_django/site_web_django/boards/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
-from .views import IndexPageView
+from .views import IndexPageView, obtenerFecha, menuView
 
 urlpatterns = [
     path('', IndexPageView.as_view(), name='index'),
+    path('fecha/<name>', obtenerFecha, name='fecha'),
+    path('menu/', menuView, name='menu'),
 ]
--- Modulo 6 (Django)/ejemplos/project_django/site_web_django/boards/views.py
+++ Modulo 6 (Django)/ejemplos/project_django/site_web_django/boards/views.py
@@ -1,7 +1,19 @@
 from django.views.generic import TemplateView
+from django.shortcuts import render
+import datetime
 
 class IndexPageView(TemplateView):
     template_name = "index.html"
+
+
+def obtenerFecha(request, name):
+    fechaActual = datetime.datetime.now()
+    context = {'fecha' : fechaActual, 'name' : str(name).capitalize}
+    return render(request, 'fecha.html', context)
+
+def menuView(request):
+    template_name = 'menu.html'
+    return render(request, template_name)
 
 """from django.shortcuts import render
 

7934c31d4f9ee70a1f18bde04a59987ccd758568
Anima <cjaccardh@gmail.com>
2024-11-20 01:46:04 -0300

    palindrome web app finished

--- Modulo 6 (Django)/REVO_EXER_5/palindromo/paliapp/urls.py
+++ Modulo 6 (Django)/REVO_EXER_5/palindromo/paliapp/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import palindromo
 
 urlpatterns = [
-    path('', palindromo, name='palindromo'),
+    path('<palabra>', palindromo, name='palindromo'),
 ]
--- Modulo 6 (Django)/REVO_EXER_5/palindromo/paliapp/views.py
+++ Modulo 6 (Django)/REVO_EXER_5/palindromo/paliapp/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 
 def palindromo(request, palabra):
-    palabra = str(palabra).lower()
-    invertido = palabra[::-1]
-    if invertido == palabra:
-        context = {palabra, 'es palindromo!'}
+    palabra_alt = str(palabra).replace(' ', '').lower()
+    invertido = palabra_alt[::-1]
+    if invertido == palabra_alt:
+        context = {'palabra' : palabra, 'mensaje' : 'es palindromo!'}
         return render(request, 'palindromo.html', context)
     else:
-        context = {palabra, 'palindromo'}
+        context = {'palabra' : palabra, 'mensaje' : 'no es palindromo :('}
         return render(request, 'palindromo.html', context)

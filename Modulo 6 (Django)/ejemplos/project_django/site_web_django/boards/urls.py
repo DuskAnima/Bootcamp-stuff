75567ab4962030c1ded5d4f08ac870f01c9ee6a8
Anima <cjaccardh@gmail.com>
2024-11-22 20:03:56 -0300

    stuff

--- Modulo 6 (Django)/ejemplos/project_django/site_web_django/boards/urls.py
+++ Modulo 6 (Django)/ejemplos/project_django/site_web_django/boards/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-from .views import IndexPageView, obtenerFecha, menuView
+from .views import IndexPageView, obtenerFecha, menuView, mostrar
 
 urlpatterns = [
     path('', IndexPageView.as_view(), name='index'),
     path('fecha/<name>', obtenerFecha, name='fecha'),
     path('menu/', menuView, name='menu'),
+    path('mostrar/', mostrar, name='mostrar')
 ]
--- Modulo 6 (Django)/ejemplos/project_django/site_web_django/boards/views.py
+++ Modulo 6 (Django)/ejemplos/project_django/site_web_django/boards/views.py
@@ -2,9 +2,22 @@ from django.views.generic import TemplateView
 from django.shortcuts import render
 import datetime
 
+
+#En este caso, como estamos trabajando con FBV (Function-Base Views), la clase solo es un constructor que
+#luego una función utilizará como parámetro
+class Persona(object): 
+    def __init__(self, nombre, apellido):
+        self.nombre = nombre
+        self.apellido = apellido
+
+def mostrar(request):
+    persona = Persona('Juan', 'Pérez')
+    items = ['Primero', 'Segundo', 'Tercero', 'Cuarto']
+    context = {'nombre' : persona.nombre, 'apellido': persona.apellido, 'items' : items}
+    return render(request, 'templateexample.html', context)
+
 class IndexPageView(TemplateView):
     template_name = "index.html"
-
 
 def obtenerFecha(request, name):
     fechaActual = datetime.datetime.now()

6c8a724d91b4b63715a817f94c026a25f9ab080f
Anima <cjaccardh@gmail.com>
2024-11-23 18:12:42 -0300

    training django

---
+++ Modulo 6 (Django)/REVO_EXER_6/books/listbook/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+class Libros():
+    def __init__(self, libro, autor, precio):
+        self.libro = libro
+        self. autor = autor
+        self.precio = precio
+
+def mostrarLibros():
+    pass
+

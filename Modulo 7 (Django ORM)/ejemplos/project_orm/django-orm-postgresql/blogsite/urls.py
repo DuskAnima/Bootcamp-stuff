1f9465638cc54df0354b78b9730cba4de262110e
Anima <cjaccardh@gmail.com>
2024-12-26 15:26:32 -0300

    Delete and update ORM practice. Action attribute practice

--- Modulo 7 (Django ORM)/ejemplos/project_orm/django-orm-postgresql/blogsite/urls.py
+++ Modulo 7 (Django ORM)/ejemplos/project_orm/django-orm-postgresql/blogsite/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('agregar/', views.agregar, name='agregar'),
+    path('agregar/agregarregistro/', views.agregarregistro, name='agregarregistro'),
+    path('eliminar/<int:id>/', views.eliminar, name='eliminar'),
+    path('actualizar/<int:id>/', views.actualizar, name='actualizar'),
+    path('actualizarregistro/<int:id>', views.actualizarregistro, name='actualizarregistro'),
 ]
--- Modulo 7 (Django ORM)/ejemplos/project_orm/django-orm-postgresql/blogsite/views.py
+++ Modulo 7 (Django ORM)/ejemplos/project_orm/django-orm-postgresql/blogsite/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.urls import reverse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from .models import Post
 
@@ -10,3 +11,39 @@ def index(request):
         'posts': posts,
     }
     return HttpResponse(template.render(context, request))
+
+def agregar(request):
+    template = loader.get_template('agregar.html')
+    return HttpResponse(template.render({}, request))
+
+def agregarregistro(request):
+    titulo = request.POST['title']
+    contenido = request.POST['content']
+    autor = request.POST['author']
+    post = Post(title=titulo, content=contenido, author=autor)
+    post.save()
+    return HttpResponseRedirect(reverse('index'))
+
+def eliminar(request, id):
+    post = Post.objects.get(id=id)
+    post.delete()
+    return HttpResponseRedirect(reverse('index'))
+
+def actualizar(request, id):
+    post = Post.objects.get(id=id)
+    template = loader.get_template('actualizar.html')
+    context = {
+        'post':post,
+    }
+    return HttpResponse(template.render(context, request))
+
+def actualizarregistro(request, id):
+    titulo = request.POST['title']
+    contenido = request.POST['content']
+    autor = request.POST['author']
+    post = Post.objects.get(id=id)
+    post.title = titulo
+    post.content = contenido
+    post.author = autor
+    post.save()
+    return HttpResponseRedirect(reverse('index'))

b94069be9f824bbf6bf00ff2514e1842526980be
Anima <cjaccardh@gmail.com>
2024-12-27 16:08:02 -0300

    Create and Read activity

--- Modulo 7 (Django ORM)/practica_orm/productos_del_proyecto/urls.py
+++ Modulo 7 (Django ORM)/practica_orm/productos_del_proyecto/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index
+from .views import index, agregar
 
 urlpatterns = [
     path('', index, name='index'),
+    path('agregar/', agregar, name='agregar'),
 ]
--- Modulo 7 (Django ORM)/practica_orm/productos_del_proyecto/views.py
+++ Modulo 7 (Django ORM)/practica_orm/productos_del_proyecto/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from .models import Producto
+from .forms import ProductoForm
 
 def index(request):
-    productos = Producto().objects.all().values()
-    return render(request, 'productos_del_proyecto/index.html', context={'productos' : productos})
+    productos = Producto.objects.all().values()
+    return render(request, 'productos_del_proyecto/index.html', 
+    context={'productos' : productos})
+
+def agregar(request):
+    if request.method == 'POST':
+        form = ProductoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse('index'))
+    else:
+        form = ProductoForm()
+    return render(request, 'productos_del_proyecto/agregar.html', context={'form' : form})
